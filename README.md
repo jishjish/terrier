2bc73dea7d28300a3e9d8c1c8806f22d54bbda4d
Josh Phillips <joshphillips@Joshs-MacBook-Pro-2.local>
2025-05-01 14:56:20 -0400

    updated readme

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Unwieldy code base? Get to know your code better with Terrier.
 - **Function tree analysis**: `cargo run -- tree -p "src/main.rs"`
 
 **Note**: After installation, replace `cargo run` with `terrier`
+
+**Note**:  Supported file types: `Rust`, `Python`.
--- src/func.rs
+++ src/func.rs
@@ -4,6 +4,7 @@
         - Rust (rs), Python (py), etc. */
 
 use std::fs;
+use std::collections;
 use std::path::PathBuf;
 use owo_colors::OwoColorize;
 use crate::utils::get_extension_from_filename;  // internal fn from utils
@@ -15,36 +16,75 @@ pub fn func_identification(filename: &PathBuf) -> Option<String> {
     let contents = fs::read_to_string(filename)
         .expect("Unable to read file.");
 
-    // match function based on file type
-    match ext.as_str() {
+    let func_declarations = collections::HashMap::from([
+        ("rs", "fn"),
+        ("py", "def")
+    ]);
+
+
+    let func_name: Option<&str> = match ext.as_str() {
         "rs" => {
             println!("{}", "Parsing Rust file...".green());
-            let mut count = 0;
-            for (i, line) in contents.lines().enumerate() {
-                if line.contains("fn") {
-                    count += 1;
-                    println!("Line {}:      {}", i+1, line.blue());
-                }
-            }
-            println!("┌──────── Summary: {:?} ────────────┐", filename.file_name().unwrap().red());
-            println!("│  - Total lines: {:<20}   │", contents.lines().count().green());
-            println!("│  - Functions: {:<24} │", count.to_string().green());
-            println!("└────────────────────────────────────────┘");
-        
+            Some(func_declarations.get("rs")?)
         },
         "py" => {
             println!("{}", "Parsing Python file...".green());
-            let mut count = 0;
-            for (i, line) in contents.lines().enumerate() {
-                if line.contains("def") {
-                    count += 1;
-                    println!("Line {}:      {}", i+1, line.blue());
-                }
-            }
+            Some(func_declarations.get("py")?)
         },
         _ => {
-            println!("Unsupported file type")
+            println!("Unsupported file type.");
+            None
+        }
+    };
+
+    println!("func name is {:?}", func_name.unwrap().to_string());
+
+    let mut count = 0;
+    for (i, line) in contents.lines().enumerate() {
+        if line.contains("fn") {
+            count += 1;
+            println!("Line {}:      {}", i+1, line.blue());
         }
     }
+    println!("┌──────── Summary: {:?} ────────────┐", filename.file_name().unwrap().red());
+    println!("│  - Total lines: {:<20}   │", contents.lines().count().green());
+    println!("│  - Functions: {:<24} │", count.to_string().green());
+    println!("└────────────────────────────────────────┘");
+
+    // // match function based on file type
+    // match ext.as_str() {
+    //     "rs" => {
+    //         println!("{}", "Parsing Rust file...".green());
+    //         let mut count = 0;
+    //         for (i, line) in contents.lines().enumerate() {
+    //             if line.contains("fn") {
+    //                 count += 1;
+    //                 println!("Line {}:      {}", i+1, line.blue());
+    //             }
+    //         }
+    //         println!("┌──────── Summary: {:?} ────────────┐", filename.file_name().unwrap().red());
+    //         println!("│  - Total lines: {:<20}   │", contents.lines().count().green());
+    //         println!("│  - Functions: {:<24} │", count.to_string().green());
+    //         println!("└────────────────────────────────────────┘");
+        
+    //     },
+    //     "py" => {
+    //         println!("{}", "Parsing Python file...".green());
+    //         let mut count = 0;
+    //         for (i, line) in contents.lines().enumerate() {
+    //             if line.contains("def") {
+    //                 count += 1;
+    //                 println!("Line {}:      {}", i+1, line.blue());
+    //             }
+    //         }
+    //         println!("┌──────── Summary: {:?} ────────────┐", filename.file_name().unwrap().red());
+    //         println!("│  - Total lines: {:<20}   │", contents.lines().count().green());
+    //         println!("│  - Functions: {:<24} │", count.to_string().green());
+    //         println!("└────────────────────────────────────────┘");
+    //     },
+    //     _ => {
+    //         println!("Unsupported file type")
+    //     }
+    // }
     None
 }
