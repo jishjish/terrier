3cb6a0cbe87d8e8c87831000fcf52fd11c9cfe52
Josh Phillips <joshphillips@Joshs-MacBook-Pro-2.local>
2025-05-02 16:01:33 -0400

    basic folder walk and file retrieval

--- src/link.rs
+++ src/link.rs
@@ -1,32 +1,52 @@
 use std::fs;
-use std::fs::read_dir;
+use std::collections::HashMap;
+// use std::fs::read_dir;
 use std::path::PathBuf;
+use owo_colors::OwoColorize;
 
 use walkdir::WalkDir;
 use crate::utils::SUPPORTED_TYPES;
 
 /// Iteration over folder to identify function interconnectedness.
 /// Pass in folder / structure to iterate through.
-pub fn link_func_search(filename: &PathBuf) {
+pub fn link_func_search(filename: &PathBuf) -> HashMap<std::string::String, std::string::String> {
 
-    // println!("supported types are : {:?}", SUPPORTED_TYPES);
+    // let mut supported_files: Vec<PathBuf> = Vec::new();
 
-    let mut supported_files: Vec<PathBuf> = Vec::new();
+    // let mut supported_files = HashMap::new();
+    let mut map = HashMap::new();
+
 
     for entry in WalkDir::new(filename) {
         let entry = entry.unwrap();
-        println!("{}", entry.path().display());
+        // println!("{}", entry.path().display());
+        // println!("entry: {:?}", entry.path());
 
-        // println!("dir entry is {:?}", entry);
+        if entry.file_type().is_file() {
+            println!("file: {:?}", entry);
 
-        
-        if entry.file_type().is_dir() {
-            ;
-        } else {
-            let contents: String = fs::read_to_string(filename)
-                .expect("Unable to read file.");
-            println!("contents are: {}", contents);
+            let file_path = entry.path();
+
+            let extension = file_path.extension().unwrap();
+            let ext_type = extension.to_string_lossy().to_string();
+            // println!("extension is {:?}", extension);
+
+            if SUPPORTED_TYPES.contains(&ext_type.as_str()) {
+                let contents: String = fs::read_to_string(file_path)
+                    .expect("Unable to read file.");
+
+                // println!("{}", "---------------------------------".red());
+                // println!("{}", contents);
+                // println!("{}", "---------------------------------".red());
+
+                map.insert(entry.file_name().to_string_lossy().to_string(), contents);
+
+            }
         }
+
     }
+
+    println!("map is {:?}", map);
+    map 
 
 }
